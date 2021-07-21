18a0b67e1d7ada9eca9a9eb6901b484fe47bfd0e
zicoooooooo <33505643@qq.com>
2021-07-21 23:35:59 +0800

    更新了HOME导航栏

--- src/router/router.js
+++ src/router/router.js
@@ -1,25 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Cart = () => import('views/cart/Cart')
+const Profile = () => import('views/profile/Profile')
 
 Vue.use(Router)
+
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes,
 })
---
+++ src/views/home/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="home"> 
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
+  export default {
+    name: "Home",
+    components:{
+      NavBar,
+    },
+    data(){
+      return {
+        
+
+      }
+    },
+    created(){
+      getHomeMultidata().then(res=>console.log(res))
+    }
+  }
+</script>
+
+<style scoped>
+
+.home-nav{
+  background-color: palevioletred;
+  color: #fff;
+}
+</style>
